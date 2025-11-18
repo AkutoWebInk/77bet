da7f727522b6fc12b8365a9c055c52ca1fb85837
AkutoWebInk <akutowebink@gmail.com>
2025-11-17 21:09:56 -0500

    Aligning

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,2 @@
-export const API_PATH = 'http://100.28.1.144/api';
+// export const API_PATH = 'http://100.28.1.144:8000/api'; //Uncomment for dev
+export const API_PATH = 'https://www.coloryfe.com/api'; //Uncomment for prod
