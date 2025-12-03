23fd4e1ea6a4771109a06657a16d9ef36e8c596a
AkutoWebInk <akutowebink@gmail.com>
2025-12-03 16:57:04 -0500

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-// export const API_PATH = 'http://192.168.1.105:8000/api'; //Uncomment for dev
-export const API_PATH = 'https://www.coloryfe.com/api'; //Uncomment for prod
+export const API_PATH = 'http://192.168.1.105:8000/api'; //Uncomment for dev
+// export const API_PATH = 'https://www.coloryfe.com/api'; //Uncomment for prod
